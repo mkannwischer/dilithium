9d9cad92a4dc91e68fc8626f4b38910ab9863b78
Thom Wiggers <thom@thomwiggers.nl>
2019-12-11 09:24:46 +0100

    Include some feedback from @bwesterb in https://github.com/pq-crystals/dilithium/pull/10

--- avx2/ntt.h
+++ avx2/ntt.h
@@ -6,21 +6,31 @@
 #include "params.h"
 #include "nttconsts.h"
 
-void DILITHIUM_ntt_levels0t2_avx(uint64_t *tmp,
-                                 const uint32_t *a,
-                                 const uint32_t *zetas);
-void DILITHIUM_ntt_levels3t8_avx(uint32_t *a,
-                                 const uint64_t *tmp,
-                                 const uint32_t *zetas);
+void DILITHIUM_ntt_levels0t2_avx(
+        uint64_t *tmp,
+        const uint32_t *a,
+        const uint32_t *zetas
+    );
+void DILITHIUM_ntt_levels3t8_avx(
+        uint32_t *a,
+        const uint64_t *tmp,
+        const uint32_t *zetas
+    );
 
-void DILITHIUM_invntt_levels0t4_avx(uint64_t *tmp,
-                                    const uint32_t *a,
-                                    const uint32_t *zetas_inv);
-void DILITHIUM_invntt_levels5t7_avx(uint32_t *a,
-                                    const uint64_t *tmp,
-                                    const uint32_t *zetas_inv);
+void DILITHIUM_invntt_levels0t4_avx(
+        uint64_t *tmp,
+        const uint32_t *a,
+        const uint32_t *zetas_inv
+    );
+void DILITHIUM_invntt_levels5t7_avx(
+        uint32_t *a,
+        const uint64_t *tmp,
+        const uint32_t *zetas_inv
+    );
 
-void DILITHIUM_pointwise_avx(uint32_t *c, const uint32_t *a, const uint32_t *b);
-void DILITHIUM_pointwise_acc_avx(uint32_t *c, const uint32_t *a, const uint32_t *b);
+void DILITHIUM_pointwise_avx(
+        uint32_t *c, const uint32_t *a, const uint32_t *b);
+void DILITHIUM_pointwise_acc_avx(
+        uint32_t *c, const uint32_t *a, const uint32_t *b);
 
 #endif
