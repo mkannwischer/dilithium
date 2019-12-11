4c29da577eea29fb9f7a88129b1260608cbd1e5c
Thom Wiggers <thom@thomwiggers.nl>
2019-12-11 08:50:22 +0100

    Don't use const integers in arguments

--- ref/rounding.h
+++ ref/rounding.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
-uint32_t DILITHIUM_power2round(const uint32_t a, uint32_t *a0);
+uint32_t DILITHIUM_power2round(uint32_t a, uint32_t *a0);
 uint32_t DILITHIUM_decompose(uint32_t a, uint32_t *a0);
-unsigned int DILITHIUM_make_hint(const uint32_t a0, const uint32_t a1);
-uint32_t DILITHIUM_use_hint(const uint32_t a, const unsigned int hint);
+unsigned int DILITHIUM_make_hint(uint32_t a0, uint32_t a1);
+uint32_t DILITHIUM_use_hint(uint32_t a, unsigned int hint);
 
 #endif
